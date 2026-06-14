77977b48336ebf34dbda5cb980cb1e91eaea5ec9
Tolnari <your-copied-noreply-email@users.noreply.github.com>
2026-06-14 16:24:11 -0500

    changes

--- on_board_temp_sensor.py
+++ on_board_temp_sensor.py
@@ -3,7 +3,7 @@ import time
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-# version 2
+# version 3
 
 # Check for updates
 firmware_url = "https://raw.githubusercontent.com/Tolnari/pico-ota-testing/"
@@ -26,4 +26,5 @@ while True:
 	time.sleep(1)
 	print(f"farenheit {round(temperature * (9/5) + 32, 1)}")
 	print(f"Celcius   {temperature}")
+	print()
 	time.sleep(4)
