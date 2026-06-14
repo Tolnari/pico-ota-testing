c250ba17676c765a366155afc29e86bd07de4670
Tolnari <your-copied-noreply-email@users.noreply.github.com>
2026-06-14 11:50:48 -0500

    refactor out the LED stuff and update version

--- onboard_temp_sensor.py
+++ onboard_temp_sensor.py
@@ -3,16 +3,18 @@ import time
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-# firmware_url = "https://https://raw.githubusercontent.com/Tolnari/pico-ota-testing/refs/heads/main/<file_name>"
+# Check for updates
+firmware_url = "https://raw.githubusercontent.com/Tolnari/pico-ota-testing/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD,firmware_url, "onboard_temp_sensor.py")
+
+ota_updater.download_and_install_update_if_available()
 
 # Do python things
 
 adcpin = 4
 sensor = machine.ADC(adcpin)
-LED = machine.Pin(25, machine.Pin.OUT)
-foo = True
-roo = False
-  
+
 def ReadTemperature():
 	adc_value = sensor.read_u16()
 	volt = (3.3/65535) * adc_value
@@ -21,11 +23,7 @@ def ReadTemperature():
   
 while True:
 	temperature = ReadTemperature()
-	#LED.toggle()
-	LED.value(foo)
 	time.sleep(1)
-	LED.value(roo)
 	print(f"Celcius {temperature}")
 	print(f"farenheit {round(temperature * (9/5) + 32, 1)}")
 	time.sleep(4)
-	print("Foo")
